5701553c54e085392a8af2e456aea4f83285d737
Hans Kristian Flaatten <hans.flaatten@evry.com>
2018-09-04 10:39:29 +0200

    Hide kube_config Terraform output from stdout

--- aks.tf
+++ aks.tf
@@ -65,6 +65,7 @@ resource "azurerm_kubernetes_cluster" "k8s_cluster" {
 
 output "kube_config" {
   value = "${azurerm_kubernetes_cluster.k8s_cluster.kube_config_raw}"
+  sensitive = true
 }
 
 output "host" {
